2b651c3c3b5577160232604e18b52cc786cb5747
Komali Remalagadda <komaliremalagadda@gmail.com>
2026-07-21 08:32:36 +0530

    Added TiDB Cloud SSL connection

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql2");
 require("dotenv").config();
+
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
 });
 
 connection.connect((err) => {
@@ -15,7 +21,7 @@ connection.connect((err) => {
     return;
   }
 
-  console.log("✅ Connected to MySQL");
+  console.log("✅ Connected to TiDB Cloud");
 });
 
 module.exports = connection;
