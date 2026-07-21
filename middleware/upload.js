bc10115e975359e5af99b81b44483ef5dbb5d0c2
Komali Remalagadda <komaliremalagadda@gmail.com>
2026-07-21 10:04:18 +0530

    Fix uploads directory creation

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,4 +1,4 @@
-const multer = require("multer");
+/* local const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
@@ -17,4 +17,31 @@ const upload = multer({
   storage,
 });
 
-module.exports = upload;
+module.exports = upload; */
+
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadPath = path.join(__dirname, "../uploads");
+
+// Create uploads folder if it doesn't exist
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadPath);
+  },
+
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
+
+    cb(null, uniqueName + path.extname(file.originalname));
+  },
+});
+
+module.exports = multer({
+  storage,
+});
